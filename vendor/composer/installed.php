a9103645ac6f76e3ef6651926b00dd0e9901acfc
Kadir <kadir@kadirgun.com>
2024-11-29 10:43:41 +0300

    Release 1.3.6

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '2f80e807883b12ecec3f97e57687af9482da1427',
+        'reference' => 'f7471ab42a4cc0249fe28b7c43b865667e36c19b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '2f80e807883b12ecec3f97e57687af9482da1427',
+            'reference' => 'f7471ab42a4cc0249fe28b7c43b865667e36c19b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
